ade9ce47f54e5b6fb83d4b3cffe6a85dd7e10c22
Anessa Petteruti <anessa_petteruti@brown.edu>
2021-10-21 13:46:20 -0400

    Commented preprocess and cleaned up data

--- preprocess.py
+++ preprocess.py
@@ -5,7 +5,11 @@ import pandas as pd
 import numpy as np
 
 def save_as_csv():
-    path = './raw_data/gdc_download_biomedical_ov/'
+    """
+    Saves all raw txt files as separate csvs to later merge.
+    """
+
+    # Biomedical data:
     read_aliquot = pd.read_csv (r'./raw_data/gdc_download_biomedical_ov/nationwidechildrens.org_biospecimen_aliquot_ov.txt')
     read_aliquot.to_csv (r'./csv_data/aliquot.csv', index=None)
 
@@ -36,9 +40,35 @@ def save_as_csv():
     read_ssf_tumor = pd.read_csv (r'./raw_data/gdc_download_biomedical_ov/nationwidechildrens.org_ssf_tumor_samples_ov.txt', error_bad_lines=False)
     read_ssf_tumor.to_csv (r'./csv_data/ssf_tumor.csv', index=None)
 
+    # Clinical data:
+    read_drug = pd.read_csv (r'./raw_data/gdc_download_clinical_ov/nationwidechildrens.org_clinical_drug_ov.txt', error_bad_lines=False)
+    read_drug.to_csv (r'./csv_data/drug.csv', index=None)
+
+    read_v1_nte = pd.read_csv (r'./raw_data/gdc_download_clinical_ov/nationwidechildrens.org_clinical_follow_up_v1.0_nte_ov.txt')
+    read_v1_nte.to_csv (r'./csv_data/v1_nte.csv', index=None)
+
+    read_v1 = pd.read_csv (r'./raw_data/gdc_download_clinical_ov/nationwidechildrens.org_clinical_follow_up_v1.0_ov.txt')
+    read_v1.to_csv (r'./csv_data/v1.csv', index=None)
+
+    read_nte = pd.read_csv (r'./raw_data/gdc_download_clinical_ov/nationwidechildrens.org_clinical_nte_ov.txt')
+    read_nte.to_csv (r'./csv_data/nte.csv', index=None)
+
+    read_omf_v4 = pd.read_csv (r'./raw_data/gdc_download_clinical_ov/nationwidechildrens.org_clinical_omf_v4.0_ov.txt', error_bad_lines=False)
+    read_omf_v4.to_csv (r'./csv_data/omf_v4.csv', index=None)
+
+    read_patient = pd.read_csv (r'./raw_data/gdc_download_clinical_ov/nationwidechildrens.org_clinical_patient_ov.txt', error_bad_lines=False)
+    read_patient.to_csv (r'./csv_data/patient.csv', index=None)
+
+    read_radiation = pd.read_csv (r'./raw_data/gdc_download_clinical_ov/nationwidechildrens.org_clinical_radiation_ov.txt', error_bad_lines=False)
+    read_radiation.to_csv (r'./csv_data/radiation.csv', index=None)
+
 
 def merge_csv():
-    path = "./csv_data/"
+    """
+    Concatenates all csvs into a single merged_bioclin_data.csv
+    """
+
+    path = "./csv_data"
 
     all_files = glob.glob(os.path.join(path, "*.csv"))
 
@@ -48,7 +78,8 @@ def merge_csv():
         all_df.append(df)
 
     merged_df = pd.concat(all_df, ignore_index=True, sort=True)
-    merged_df.to_csv (r'./biomed_data.csv', index=None)
+    merged_df.to_csv (r'./merged_bioclin_data.csv', index=None)
+
 
 def main():
     save_as_csv()
